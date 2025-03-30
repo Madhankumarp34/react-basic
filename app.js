dc1f17e681d465a04528dff0673797d8524c1ab8
madhankumarp34 <madhankumarp34@gmail.com>
2025-03-30 09:26:22 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("h1",{id:"parent"},
+  [React.createElement("h1",{id:"child"},
+  
+    [React.createElement("h3",{}, "Grand child is me"),
+    React.createElement("h2",{}, "Grand child is me")]),
+    React.createElement("h1",{},"Hello World")])
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
+console.log(parent)
