2e4183fe2a8c16be75e8b20e8c152c6fa13bbd1b
madhankumarp34 <madhankumarp34@gmail.com>
2025-03-30 12:23:11 +0530

    here are changes

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id:"parent"},[
-  React.createElement("div", {id:"child"},[
-    React.createElement("h1",{},"This is Madhan"),
-    React.createElement("h1",{},"TCS"),
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child", key: "child1" }, [
+    React.createElement("h1", { key: "h1-1" }, "This is Madhan"),
+    React.createElement("h1", { key: "h1-2" }, "TCS"),
   ]),
 
-  React.createElement("div", {id:"child2"},[
-    React.createElement("h1",{},"This is Madhan1"),
-    React.createElement("h1",{},"TCS1"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "h1-3" }, "This is Madhan1"),
+    React.createElement("h1", { key: "h1-4" }, "TCS1"),
   ]),
 ]);
 
-console.log(parent);  //object
-const root = ReactDOM.createRoot(document.createElement("root"))
+console.log(parent); //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
