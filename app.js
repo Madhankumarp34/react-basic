19661fbe6be37f88b30f7d63247d6d2767b15df8
madhankumarp34 <madhankumarp34@gmail.com>
2025-04-03 21:40:03 +0530

    changes

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child", key: "child1" }, [
-    React.createElement("h1", { key: "h1-1" }, "This is Madhan"),
-    React.createElement("h1", { key: "h1-2" }, "TCS"),
-  ]),
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child", key: "child1" }, [
+//     React.createElement("h1", { key: "h1-1" }, "This is Madhan"),
+//     React.createElement("h1", { key: "h1-2" }, "TCS"),
+//   ]),
 
-  React.createElement("div", { id: "child2", key: "child2" }, [
-    React.createElement("h1", { key: "h1-3" }, "This is Madhan1"),
-    React.createElement("h1", { key: "h1-4" }, "TCS1"),
-  ]),
-]);
+//   React.createElement("div", { id: "child2", key: "child2" }, [
+//     React.createElement("h1", { key: "h1-3" }, "This is Madhan1"),
+//     React.createElement("h1", { key: "h1-4" }, "TCS1"),
+//   ]),
+// ]);
 
-console.log(parent); //object
+// console.log(parent); //object
+
+const InjectComponent = () =>(
+  <p>Hi This is Madhan Kumar</p>
+)
+  
+
+
+const Headingcomponent = () => (
+  <div id="one" className="Hello">  
+    <h1>Hello World</h1>
+  <InjectComponent/> 
+  <InjectComponent></InjectComponent>
+  {
+    InjectComponent()
+  }
+ </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Headingcomponent />);
